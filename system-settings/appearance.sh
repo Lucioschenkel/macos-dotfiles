f6c461d86fb09bcca233aff9acb04654ccc372b3
Lucioschenkel <lucio.schenkel@proton.me>
2025-08-24 15:49:28 -0300

    feat: add wallpaper and mission control view

--- system-settings/appearance.sh
+++ system-settings/appearance.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# --- Dock --- #
+
 # Auto-hide MacOS Dock
-defaults write com.apple.dock autohide -bool true && killall Dock
+defaults write com.apple.dock autohide -bool true
+
+# Group windows by app so we don't end up with tiny windows because of AeroSpace
+defaults write com.apple.dock expose-group-apps -bool true
+
+# Restart the Dock
+killall Dock
+
+# --- Wallpaper --- #
+WALLPAPER=$HOME/dotfiles/assets/purple-mountains-dear.png
+WALLPAPER_ABS_PATH=$(realpath "$WALLPAPER")
+osascript -e "tell application \"System Events\" to set picture of every desktop to POSIX file \"$WALLPAPER_ABS_PATH\""
